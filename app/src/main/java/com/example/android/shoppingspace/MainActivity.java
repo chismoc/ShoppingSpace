c30936b69a4e27286bdd76684946efc7b19be927
chismoc <chisomo.chingwalu@yahoo.co.uk>
2021-11-10 21:37:02 +0200

    main fragment created

--- app/src/main/java/com/example/android/shoppingspace/MainActivity.java
+++ app/src/main/java/com/example/android/shoppingspace/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.shoppingspace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
@@ -36,6 +40,25 @@ public class MainActivity extends AppCompatActivity {
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        
+        //add onclickListener
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.cart:
+                        Toast.makeText(MainActivity.this, "Cart Selected", Toast.LENGTH_SHORT).show();
+               break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+        //Create fragment Transaction
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container,new MainFragment());
+        transaction.commit();
     }
 
     private void initViews() {
