956c9c333a1cccfacd3d65b2df525b823b20f3b0
chismoc <chisomo.chingwalu@yahoo.co.uk>
2021-11-10 21:00:00 +0200

    navigation Drawer finalised

--- app/src/main/java/com/example/android/shoppingspace/MainActivity.java
+++ app/src/main/java/com/example/android/shoppingspace/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.android.shoppingspace;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Initialize View Items
+
+    private View items;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize Views method
+        initViews();
+
+        //set toolbar
+        setSupportActionBar(toolbar);
+
+        //set toggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.drawer_open, R.string.drawer_closed);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void initViews() {
+        drawer = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolBar);
     }
 }
