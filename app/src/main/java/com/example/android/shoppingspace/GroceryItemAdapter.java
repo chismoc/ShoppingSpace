bb49710bcae663b80656928e3dfeef6d6fbf0f00
chismoc <chisomo.chingwalu@yahoo.co.uk>
2021-11-12 14:54:57 +0200

    GroceryItemAdapter set

---
+++ app/src/main/java/com/example/android/shoppingspace/GroceryItemAdapter.java
@@ -0,0 +1,82 @@
+package com.example.android.shoppingspace;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.ArrayList;
+
+public class GroceryItemAdapter extends RecyclerView.Adapter<GroceryItemAdapter.ViewHolder> {
+    private ArrayList<GroceryItem> items = new ArrayList<>();
+    private Context context;
+
+    public GroceryItemAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //instance of viewHolder
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grocery_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.itemName.setText(items.get(position).getName());
+        holder.itemPrice.setText((int) items.get(position).getPrice());
+
+        //set image
+        Glide.with(context)
+                .asBitmap()
+                .load(items.get(position).getImageUrl())
+                .into(holder.itemImage);
+
+        //set onClickListener
+
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:add action
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView itemName, itemPrice;
+        private ImageView itemImage;
+        private MaterialCardView parent;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemName = itemView.findViewById(R.id.itemName_textView);
+            itemPrice = itemView.findViewById(R.id.itemPrice_textView);
+            itemImage = itemView.findViewById(R.id.itemImage_img);
+            parent = itemView.findViewById(R.id.parent);
+        }
+    }
+
+    public void setItems(ArrayList<GroceryItem> items) {
+        this.items = items;
+
+        notifyDataSetChanged();
+    }
+}
